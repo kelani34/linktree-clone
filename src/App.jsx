be8947daeca8b60dd4404d13c12c511ccb31f151
kelani34 <kelanitaiwo34@gmail.com>
2022-10-22 14:29:15 +0100

    Add links

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Link } from 'react-router-dom'
 import './App.css'
+import profileImg from './assets/images/profile__img.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      <>
+        <div>
+          <div className='m-10 flex flex-col justify-center items-center'>
+            <img id='profile__img' src={profileImg}/>
+            <h2 className='mt-5 font-bold'>Annette Black</h2>
+          </div>
+          <div className='items-center'>
+            <a href='https://twitter.com'> <div className='rounded-lg  m-5 bg-slate-500 p-4 text-center'> Twitter Link</div></a>
+          </div>
+        </div>
+      
+      </>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{jsx,js,ts,tsx}"],
   theme: {
     extend: {},
   },

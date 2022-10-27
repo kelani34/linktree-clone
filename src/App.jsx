9d91e8ec4bec8c2cea9743a326872be77785b3f1
kelani34 <kelanitaiwo34@gmail.com>
2022-10-27 16:17:38 +0100

    Fix: menubar and share button

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,25 @@ import './App.css'
 import profileImg from './assets/images/profile__img.png'
 import slackIcon from './assets/icons/slack-icon.png'
 import githubIcon from './assets/icons/github-icon.png'
+import shareIcon from './assets/icons/share-icon.svg'
+import menuIcon from './assets/icons/menu-icon.svg'
 import Footer from './components/Footer'
 import Links from './components/Links'
+import { FacebookIcon, TwitterIcon, WhatsappIcon,} from "react-share";
+import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from "react-share";
 
 function App() {
   const [link,  setLink] = useState([
     {
       id: 'twitter',
       text: 'Twiter Link',
+      display: 'block',
+      link: 'https://twitter.com/kelanicodes'
+    },
+    {
+      id: 'slack',
+      text: 'Taiwo Shina-kelani',
+      display: 'hidden',
       link: 'https://twitter.com/kelanicodes'
     },
     {
@@ -22,47 +33,63 @@ function App() {
     {
       id: 'books',
       text: 'Books',
+      display: 'block',
       link: 'https://books.zuri.team'
     },
     {
       id: 'book__python',
       text: 'Twiter Link',
+      display: 'block',
       link: 'https://books.zuri.team/python-for-beginners?ref_id=kelanitaiwo34'
     },
     {
       id: 'pitch',
       text: 'Pitch',
+      display: 'block',
       link: 'https://background.zuri.team'
     },
     {
       id: 'book__design',
-      text: '',
+      text: 'Design books',
+      display: 'block',
       link: 'https://books.zuri.team/design-rules'
     },
   ])
   return (
       <>
         <div>
-            <div className='m-10 mt-25 flex flex-col justify-center items-center'>
-              <img id='profile__img' src={profileImg}/>
-              <h2 className='mt-5 font-bold'>Taiwo Shina-Kelani</h2>
-            </div>
-            { link.map((link) => {
-              return (
-                <Links 
-                id={link.id}
-                text= {link.text}
-                link={link.link}
-                />
-              )
-            }) 
-          }
-          <div className='flex mt-4 justify-center space-x-6'>
-            <img src={slackIcon} />
-            <img src={githubIcon} />
+          
+          <div className='m-10 mt-25 flex flex-col justify-center items-center relative'>
+            
+            <img id='profile__img' src={profileImg} className='profile-img'/>
+            <h2 className='mt-5 font-bold'>Taiwo Shina-Kelani</h2>
+            <div class="tooltip">
+              <img src={shareIcon} className='absolute share-button'/>
+              <TwitterShareButton url={"Checkout kehlani on https://linktr-ee.netlify.app"} className='absolute share-button opacity-0 twitter'>
+                  <TwitterIcon size={32} round={true} />
+              </TwitterShareButton>
+              <span className="tooltiptext">Share button</span>
           </div>
-      </div>
-      <Footer />
+          <img src={menuIcon} className='absolute menu-icon'/>
+          </div>
+          { link.map((link) => {
+            return (
+              <Links 
+              adding the target="_blank"
+              id={link.id}
+              text= {link.text}
+              link={link.link}
+              display={link.display}
+              />
+            )
+          }) 
+        }
+        <div className='flex mt-4 justify-center space-x-6'>
+          <a><img src={slackIcon} /></a>
+          <a><img src={githubIcon} /></a>
+        </div>
+    </div>
+    <Footer />
       </>
   )
 }
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Links(props) {
   return (
     <div className='  items-center m-4'>
-            <a href={props.link} id={props.id}> <div className=' rounded-lg p-4 text-center links'> {props.text}</div></a>
+            <a href={props.link} id={props.id} target="_blank" rel="noreferrer noopener" classname= {props.display}> <div className=' rounded-lg p-4 text-center links'> {props.text}</div></a>
     </div>
   )
 }

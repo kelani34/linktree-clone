e3e8576b98346e8cf0c841e1aa3ab534ca03160e
kelani34 <kelanitaiwo34@gmail.com>
2022-10-27 07:11:55 +0100

    Add horizontal line

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,9 @@ import zuri__logo from '../assets/images/zuri__logo.png'
 function Footer() {
   return (
     <div className=''>
+      <hr/>
         <div className='p-6  flex  footer'>
+          
         <img src={zuri__logo} />
         <span className='text-slate-400 '>HNG Internship 9 Frontend Task</span>
         <img src={I4G__logo} className=''/>
